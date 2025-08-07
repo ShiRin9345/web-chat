edc4d3f440b90ed5c6be21eff8bf2ee37f039e24
ShiRin <2463951865z@gmail.com>
2025-08-07 17:39:10 +0800

    ready to use useMutation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import ChatHeader from '@/components/chatHeader.tsx'
 import ChatInput from '@/components/chatInput.tsx'
 import socket from '@/lib/socket.ts'
 import { useChatStore } from '@/store/chatStore.ts'
+import { db } from '@/lib/db.ts'
 
 export const Route = createFileRoute('/')({
+  beforeLoad: () => ({
+    chatMessagesQueryOptions: {
+      queryKey: ['chat-messages'],
+      queryFn: async () => {
+        const messages = await db.message.findMany()
+        return messages
+      },
+    },
+  }),
   component: Home,
+  loader: ({ context }) => {
+    context.queryClient.prefetchQuery(context.chatMessagesQueryOptions)
+  },
 })
 
 function Home() {
-  const messages = useChatStore((state) => state.messages)
+  const { chatMessagesQueryOptions } = Route.useRouteContext()
+  const { data: messages } = useQuery(chatMessagesQueryOptions)
   const addMessage = useChatStore((state) => state.addMessage)
   useEffect(() => {
     socket.on('received', addMessage)
@@ -22,8 +37,8 @@ function Home() {
     <main className="flex flex-col h-full">
       <ChatHeader />
       <div className="p-2 flex-1 h-full overflow-auto">
-        {messages.map((message) => (
-          <MessageItem>{message}</MessageItem>
+        {messages?.map((message) => (
+          <MessageItem>{message.content}</MessageItem>
         ))}
       </div>
       <div className="flex  items-center justify-center">
