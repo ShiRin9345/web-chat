0af34e48081c1e639a17d9d9c9ca3d5249330518
ShiRin <2463951865z@gmail.com>
2025-08-17 14:17:04 +0800

    use Status to subsititute indicator

--- src/components/indicator.tsx
+++ src/components/indicator.tsx
@@ -1,23 +1,31 @@
-import { Badge } from '@/components/ui/badge.tsx'
 import { useSocket } from '@/providers/socketProvider.tsx'
+import {
+  Status,
+  StatusIndicator,
+  StatusLabel,
+} from '@/components/ui/shadcn-io/status'
 import { cn } from '@/lib/utils.ts'
 
 const Indicator = () => {
   const { isConnected } = useSocket()
   return (
-    <Badge
-      className={cn(
-        'w-fit self-end rounded-full text-[0.6rem] text-white',
-        isConnected ? 'bg-emerald-500' : 'bg-amber-500',
-      )}
-      variant={
-        cn(isConnected ? 'destructive' : 'secondary') as
-          | 'destructive'
-          | 'secondary'
-      }
-    >
-      {isConnected ? '在线' : '离线'}
-    </Badge>
+    // <Badge
+    //   className={cn(
+    //     'w-fit self-end rounded-full text-[0.6rem] text-white',
+    //     isConnected ? 'bg-emerald-500' : 'bg-amber-500',
+    //   )}
+    //   variant={
+    //     cn(isConnected ? 'destructive' : 'secondary') as
+    //       | 'destructive'
+    //       | 'secondary'
+    //   }
+    // >
+    //   {isConnected ? '在线' : '离线'}
+    // </Badge>
+    <Status status={`${isConnected ? 'online' : 'offline'}`}>
+      <StatusLabel />
+      <StatusIndicator />
+    </Status>
   )
 }
 export default Indicator
