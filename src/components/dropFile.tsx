f3ff2befd82a02193a18fa16d2e38eef323941cb
ShiRin <2463951865z@gmail.com>
2025-08-21 16:34:43 +0800

    begin to fix the dropzone bug

---
+++ src/components/dropFile.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import { useDropzone } from 'react-dropzone'
+import { ImagePlus } from 'lucide-react'
+import type { OssInfo } from '@/components/ImageDialog.tsx'
+import { messageType } from '@/components/chatInput.tsx'
+import { cn } from '@/lib/utils.ts'
+import { useEffect } from 'react'
+
+const DropFile = () => {
+  const onDrop = async (uploadFiles: Array<File>) => {
+    const file = uploadFiles[0]
+    const response = await axios.get<OssInfo>('/api/oss-signature')
+    const ossInfo = response.data
+    const formdata = new FormData()
+
+    formdata.append('key', file.name)
+    formdata.append('OSSAccessKeyId', ossInfo.OSSAccessKeyId)
+    formdata.append('policy', ossInfo.policy)
+    formdata.append('signature', ossInfo.Signature)
+    formdata.append('success_action_status', '200')
+    formdata.append('file', file)
+    await axios.post(ossInfo.host, formdata)
+    const targetUrl = ossInfo.host + '/' + file.name
+    await axios.post('/api/groupMessages', {
+      content: targetUrl,
+      type: messageType.IMAGE,
+    })
+  }
+  const { getRootProps, isDragActive } = useDropzone({
+    accept: {
+      'image/*': ['.jpeg', '.png', '.gif'],
+    },
+    onDrop,
+    noDragEventsBubbling: true,
+  })
+
+  return (
+    <div
+      {...getRootProps()}
+      className={cn(
+        'absolute inset-0 transition-all z-50 duration-500 bg-transparent backdrop-blur-lg',
+        !isDragActive && 'invisible opacity-0',
+      )}
+    >
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[35rem] h-[13rem] bg-white flex flex-col items-center justify-center rounded-lg gap-2 py-4">
+        <ImagePlus />
+        <p className="font-semibold">Drag and drop your file here.</p>
+      </div>
+    </div>
+  )
+}
+export default DropFile
--- src/routes/(main)/group/$groupId.lazy.tsx
+++ src/routes/(main)/group/$groupId.lazy.tsx
@@ -1,21 +1,9 @@
-import { useEffect, useRef } from 'react'
-import { useVirtualizer } from '@tanstack/react-virtual'
 import { createLazyFileRoute, useParams } from '@tanstack/react-router'
-import { useInfiniteQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import { useUser } from '@clerk/clerk-react'
-import { ImagePlus, Loader } from 'lucide-react'
-import { useDropzone } from 'react-dropzone'
-import type { UserResource } from '@clerk/types'
-import type { OssInfo } from '@/components/ImageDialog.tsx'
-import type { GroupMessage } from 'generated/index'
-import useChatSocket from '@/hooks/useChatSocket.ts'
 import ChatHeader from '@/components/chatHeader.tsx'
-import ChatInput, { messageType } from '@/components/chatInput.tsx'
+import ChatInput from '@/components/chatInput.tsx'
 import PendingPage from '@/components/pendingPage.tsx'
-import { MessageItem } from '@/components/messageItem.tsx'
-import { cn } from '@/lib/utils.ts'
-import { chatMessageInfiniteQueryOptions } from '@/features/reactQuery/options.ts'
+import VirtualChatList from '@/components/virtualChatList.tsx'
+import DropFile from '@/components/dropFile.tsx'
 
 export const Route = createLazyFileRoute('/(main)/group/$groupId')({
   component: Home,
@@ -24,144 +12,11 @@ export const Route = createLazyFileRoute('/(main)/group/$groupId')({
 
 export default function Home() {
   const { groupId } = useParams({ from: '/(main)/group/$groupId' })
-  const { user } = useUser()
-
-  useChatSocket(groupId, ['messages', groupId])
-
-  const parentRef = useRef<HTMLDivElement>(null)
-  const { data, isFetchingNextPage, hasNextPage, fetchNextPage, status } =
-    useInfiniteQuery(chatMessageInfiniteQueryOptions({ groupId }))
-
-  const messages = data
-    ? data.pages.flatMap((page) => page.messages as Array<GroupMessage>)
-    : []
-  const rowVirtualizer = useVirtualizer({
-    count: hasNextPage ? messages.length + 1 : messages.length,
-    getScrollElement: () => parentRef.current,
-    estimateSize: (index) => {
-      const message = messages[index]
-      return message?.type === 'IMAGE' ? 150 : 80
-    },
-    measureElement: (element) => {
-      return element.getBoundingClientRect().height
-    },
-    overscan: 5,
-  })
-  useEffect(() => {
-    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse()
-    if (!lastItem) return
-    if (
-      lastItem.index >= messages.length - 1 &&
-      hasNextPage &&
-      !isFetchingNextPage
-    ) {
-      fetchNextPage()
-    }
-  }, [
-    rowVirtualizer.getVirtualItems(),
-    hasNextPage,
-    isFetchingNextPage,
-    messages.length,
-    fetchNextPage,
-  ])
-  const onDrop = async (uploadFiles: Array<File>) => {
-    const file = uploadFiles[0]
-    const response = await axios.get<OssInfo>('/api/oss-signature')
-    const ossInfo = response.data
-    const formdata = new FormData()
-
-    formdata.append('key', file.name)
-    formdata.append('OSSAccessKeyId', ossInfo.OSSAccessKeyId)
-    formdata.append('policy', ossInfo.policy)
-    formdata.append('signature', ossInfo.Signature)
-    formdata.append('success_action_status', '200')
-    formdata.append('file', file)
-    await axios.post(ossInfo.host, formdata)
-    const targetUrl = ossInfo.host + '/' + file.name
-    await axios.post('/api/groupMessages', {
-      groupId,
-      content: targetUrl,
-      type: messageType.IMAGE,
-    })
-  }
-  const { getRootProps, isDragActive } = useDropzone({
-    accept: {
-      'image/*': ['.jpeg', '.png', '.gif'],
-    },
-    onDrop,
-    noDragEventsBubbling: true,
-  })
   return (
-    <div {...getRootProps()} className="flex relative flex-col h-screen">
+    <div className="flex relative flex-col h-screen">
+      <DropFile />
       <ChatHeader roomId={groupId} />
-      <div
-        {...getRootProps()}
-        className={cn(
-          'absolute inset-0 transition-all z-50 duration-500 bg-transparent backdrop-blur-lg',
-          !isDragActive && 'invisible opacity-0',
-        )}
-      >
-        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[35rem] h-[13rem] bg-white flex flex-col items-center justify-center rounded-lg gap-2 py-4">
-          <ImagePlus />
-          <p className="font-semibold">Drag and drop your file here.</p>
-        </div>
-      </div>
-
-      <div
-        ref={parentRef}
-        id="topDiv"
-        className="p-2 flex-1 h-full overflow-auto bg-zinc-100 scrollbar-none relative "
-      >
-        <div
-          style={{
-            height: `${rowVirtualizer.getTotalSize()}px`,
-            width: '100%',
-            position: 'relative',
-          }}
-          className={`h-[${rowVirtualizer.getTotalSize()}px] w-full relative`}
-        >
-          {status === 'pending' ? (
-            <PendingPage />
-          ) : (
-            rowVirtualizer.getVirtualItems().map((virtualRow) => {
-              const message = messages[virtualRow.index]
-              const isLoaderRow = virtualRow.index > messages.length - 1
-              return (
-                <div
-                  key={virtualRow.key}
-                  style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: `${virtualRow.size}px`,
-                    transform: `translateY(${virtualRow.start}px)`,
-                  }}
-                >
-                  {isLoaderRow ? (
-                    hasNextPage ? (
-                      <div className="w-full  flex items-center justify-center">
-                        <Loader className="animate-spin" />
-                      </div>
-                    ) : (
-                      'Nothing more to load...'
-                    )
-                  ) : (
-                    <MessageItem
-                      index={virtualRow.index}
-                      ref={rowVirtualizer.measureElement}
-                      content={message?.content}
-                      type={message.type}
-                      user={user as UserResource}
-                    />
-                  )}
-                </div>
-              )
-            })
-          )}
-        </div>
-        <div id="bottom" />
-      </div>
+      <VirtualChatList groupId={groupId} />
       <ChatInput groupId={groupId} />
     </div>
   )
